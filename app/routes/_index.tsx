08fa6208b68fd225a67709e911204d37dcda4752
stephensavoia <ssavoia@gmail.com>
2024-06-14 14:17:05 -0400

    testing db again

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,6 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
   let results = await context.cloudflare.env.DB.prepare(
     "SELECT * FROM rides"
   ).first();
-  console.log(results);
   const url = new URL(request.url);
   const page = Number(url.searchParams.get("page")) || 1;
 
