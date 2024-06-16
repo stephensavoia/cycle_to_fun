ab1a68f1cc2e62e12a238ad650a09ae341c3411f
stephensavoia <ssavoia@gmail.com>
2024-06-16 00:59:25 -0400

    I'm so dumb I don't know how to make a local db with cloudflare d1 after 2 full days of trying

--- app/routes/dbtest.$ride.tsx
+++ app/routes/dbtest.$ride.tsx
@@ -21,7 +21,7 @@ import Ride from "~/components/Ride";
 // }
 
 export async function loader({ params, context }: LoaderFunctionArgs) {
-  console.log(context);
+  const hello = context.cloudflare.env.DB;
   console.log(params);
   const { ride } = params;
   if (ride === undefined) throw new Response("Ride is undefined");
@@ -33,20 +33,22 @@ export async function loader({ params, context }: LoaderFunctionArgs) {
 
   const data = database.getRideBySlug(ride);
   if (!data) throw new Response("Ride not found", { status: 404 });
-  return data;
+  return { data, hello };
 }
 
 export const meta: MetaFunction<typeof loader> = ({ matches, data }) => {
   const parentMeta = matches.flatMap((match) => match.meta ?? []);
-  const metaTitle = data?.title ? data?.title : "Search | Cycle TO Fun";
-  const metaDescription = data?.description
-    ? data?.description
+  const metaTitle = data?.data.title
+    ? data?.data.title
+    : "Search | Cycle TO Fun";
+  const metaDescription = data?.data.description
+    ? data?.data.description
     : "Our mission is to make cycling in Toronto as safe, accessible, and FUN as possible. Explore our collection of cycling routes and find the perfect one for your next adventure.";
-  const metaUrl = data?.slug
-    ? `https://www.cycletofun.com/${data?.slug}`
+  const metaUrl = data?.data.slug
+    ? `https://www.cycletofun.com/${data?.data.slug}`
     : "https://www.cycletofun.com/";
-  const metaImg = data?.imageUrl
-    ? `https://www.cycletofun.com${data?.imageUrl}`
+  const metaImg = data?.data.imageUrl
+    ? `https://www.cycletofun.com${data?.data.imageUrl}`
     : "https://www.cycletofun.com/img/og-image.jpg";
 
   return [
@@ -63,23 +65,24 @@ export const meta: MetaFunction<typeof loader> = ({ matches, data }) => {
 
 export default function RideBySlug() {
   const data = useLoaderData<typeof loader>();
+  console.log(data.hello);
 
   return (
     <div className="main-container ride-page-container">
       <div className="flex flex-col items-center">
         <Ride
-          key={data.id}
-          title={data.title}
-          description={data.description}
-          mapUrl={data.mapUrl}
-          tags={data.tags}
-          duration={data.duration}
-          distance={data.distance}
-          difficulty={data.difficulty}
-          routeType={data.routeType}
-          imageUrl={data.imageUrl}
-          altText={data.altText}
-          slug={data.slug}
+          key={data.data.id}
+          title={data.data.title}
+          description={data.data.description}
+          mapUrl={data.data.mapUrl}
+          tags={data.data.tags}
+          duration={data.data.duration}
+          distance={data.data.distance}
+          difficulty={data.data.difficulty}
+          routeType={data.data.routeType}
+          imageUrl={data.data.imageUrl}
+          altText={data.data.altText}
+          slug={data.data.slug}
         />
       </div>
     </div>
